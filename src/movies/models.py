562a0f06be9ac54cec01386582bb1df3ce6298ae
roman <AlexandrovRomanisi@ya.ru>
2021-01-27 21:23:32 +0400

    Add Person

--- src/movies/models.py
+++ src/movies/models.py
@@ -3,6 +3,20 @@ from django.core.validators import MaxLengthValidator, MinLengthValidator, MinVa
 from django.db import models
 
 
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=100)
+
+    birth_date = models.DateField()
+
+    @property
+    def fullname(self):
+        return f'{self.name} {self.surname}'
+
+    def __str__(self):
+        return self.fullname
+
+
 class Score(models.Model):
     value = models.IntegerField(validators=[MinLengthValidator(1), MaxLengthValidator(10)])
     user = models.ForeignKey(User, models.CASCADE, related_name="movie_score")
