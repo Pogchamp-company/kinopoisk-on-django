60ce22573fc04be0c805fa6872284a4558f591d2
rusty <golovinartem2004@gmail.com>
2021-03-30 21:05:49 +0400

    News loads from news model

--- src/news/views.py
+++ src/news/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from movies.models import Movie
 
+from news.models import News
+
 
 def index(request):
     context = dict(
-        preview=Movie.objects.order_by("-id")[:3]
+        preview=Movie.objects.order_by("-id")[:3],
+        news=News.objects.order_by("-id")[:3]
     )
 
     return render(request, 'home_page.html', context)
