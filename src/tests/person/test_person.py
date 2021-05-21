9e2a9f13d462c9dc9298c038ddd9dc77a5ef7bbc
rusty <golovinartem2004@gmail.com>
2021-05-21 22:58:19 +0400

    person tests updated

--- src/tests/person/test_person.py
+++ src/tests/person/test_person.py
@@ -1,16 +1,21 @@
 import pytest
 from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
 from person.models import Person
 
 
 @pytest.mark.django_db
 class TestPersons:
-    def test_person(self, client):
-        person1 = Person.objects.first()
+    def test_existing_person(self, client):
+        person = Person.objects.first()
+        assert person is not None
+        response = client.get(reverse('person.person_page', kwargs=dict(person_id=person.id)))
+        assertTemplateUsed(response, 'person/person.html')
+        assert response.status_code == 200
+
+    def test_not_existing_person(self, client):
         person2 = Person.objects.last()
-        assert person1 is not None
         assert person2 is not None
 
-        assert client.get(reverse('person.person_page', kwargs=dict(person_id=person1.id))).status_code == 200
         assert client.get(reverse('person.person_page', kwargs=dict(person_id=person2.id + 1))).status_code == 404
